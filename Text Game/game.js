aa73063eecd07d40208ae2e7e4499a911b352e8e
Keaton <Keaton.h143@stu.nebo.edu>
2019-02-04 10:10:29 -0700

    Fixing Bugs/ Inventory
    Fixed Bugs/ errors

--- Text Game/game.js
+++ Text Game/game.js
@@ -5,14 +5,24 @@ Multiline comment
 
 */
 
-// document.write("<h1>I can't wait for <em>spring!</em></h1>");
-
 
 // alert("Warning! Will Robinson...Warning!");
 
 // confirm("Do you like pokemon?");
 
 // prompt("What type of pokemon do you like?");
+var inventory = {
+    sword:0,
+    food:0,
+    map:0,
+    bow:0,
+    health:100,
+    ZeckberchulinPond:20,
+    SwordDamage:20,
+    ZeckberchulinBow:30,
+    ZeckberchulinBoss:50,
+}
+
 Game();
 
 function Game(){
@@ -29,37 +39,58 @@ function Game(){
             var prisonLook = prompt("The prison is small, dirty and damp. A rat scurries into a hole in the corner. Bugs squirm and scamper underfoot. There is a barred window to the back. An occupied bed has a stranger asleep snoring to the right. The cell bars and locked door to the front. An empty bed which you had sleeped in the night before to the right. In the center of the dirt floor is a lumpy moldy rug. /n -wake stranger /n -move rug /n -eat bugs").toLowerCase();
             if(prisonLook == "eat bugs" || prisonLook == "bugs" || prisonLook == "eat"){
                 var prisonsick = prompt("You find a black millipede runing along the floor, you eat it and instantly regreat it. You have been poisoned and know are puking blook all over the floor. -/look for antidote -/accept fate").toLowerCase(); 
-                if(prisonsick = "antidote"){
+                if(prisonsick == "antidote" || prisonsick == "look for antidote" || prisonsick == "look"){
                     var prisonantidote = prompt("You find a green vile on the floor next to you. /n -use it /n - don't use it");
+                    if(prisonantidote == "use it" || prisonantidote == "use"){
+                       alert("You drink your roomates sweat it burn in your mouth as you final breath escapes");
+                       var resume4 = confrim("Do you wish to continue?");
+                        
+                       if(resume4){
+                            Prison();
+                       }
+                       else{
+                            ("See Ya");
+                       }
+                   }
+                 }
                 }
-                else if(prisonsick == "fate" || prisonsick == "accept fate"){
+                else if(prisonsick == "fate" || prisonsick == "accept fate"){   
                     alert("You die from poisoning");
-                    var resume = confirm("Do you wish to continue?");
+                    var resume2 = confirm("Do you wish to continue?");
                     
-                    if(resume){
+                    if(resume2){
                         Prison();
                     }
                     else{
                         alert("QUITTER");
                     }
-                }
-                else{
-                    alert("Look at you options again");
-                }
-            }
+                
+              }
             else if(prisonLook == "wake stranger" || prisonLook == "stranger" || prisonLook == "wake"){
                 alert("You wake up a green beast with yellow eyes and sharp teeth he jumps you and stabs you");
-            }
+                var resume3 = confrim("Do you wish to continue?");
+                
+                if(resume3){
+                    Prison();
+                }
+                else{
+                    alert("Failure");
+                }
+              }   
             else if(prisonLook == "move rug" || prisonLook == "rug" || prisonLook == "move"){
                 var prisonSword = prompt("You find a sword, what will you use it on? /n -roomate /n -guard");
+                inventory.sword++
                     if(prisonSword = "roomate"){
                         var prisonRoomDead = alert("You stabed you rommate to death it looks like a green human seized lizard");
                     }
                 }
+            else{
+                ("Look at your options again");
             }
+        }
         
         else if(prison == "go to sleep" || prison == "sleep" || prison == "bed"){
-        alert("You fall back to sleep and stabbed to death by your roomate");
+        alert("You fall back to sleep and stabbed to death by your roomate")
         var resume = confirm("Do you wish to continue?");
             
         if(resume){
@@ -72,6 +103,9 @@ function Game(){
         
         else if(prison == "taunt the guard" || prison == "taunt" || "guard"){
             var gaurdTaunt = prompt("How? /n -wistle /n -yell /n -talk.");
+        }
+        else{
+            ("Look at your options again");
         }
     }
 
