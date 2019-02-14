b3667d596350d80a2bff4400a58097209afaa946
Keaton <Keaton.h143@stu.nebo.edu>
2019-02-14 10:29:42 -0700

    Fixed everything
    Made first escape

--- Text Game/game.js
+++ Text Game/game.js
@@ -35,24 +35,24 @@ function Game(){
     Prison();
     
     function Prison(){
-        var prison = prompt("You wake up...our head is pounding and your vison is blurred. After pacing around walking off the headache, your vison clears. It appears you are in prison and you cannot remember why. /n -look around /n - go back to sleep /n - taunt the guard").toLowerCase();
+        var prison = prompt("You wake up...our head is pounding and your vison is blurred. After pacing around walking off the headache, your vison clears. It appears you are in prison and you cannot remember why. /n - look around /n - go back to sleep /n - taunt the guard").toLowerCase();
         
         if(prison === "look around" || prison === "look"){
             
-            var prisonLook = prompt("The prison is small, dirty and damp. A rat scurries into a hole in the corner. Bugs squirm and scamper underfoot. There is a barred window to the back. An occupied bed has a stranger asleep snoring to the right. The cell bars and locked door to the front. An empty bed which you had sleeped in the night before to the right. In the center of the dirt floor is a lumpy moldy rug. /n -wake stranger /n -move rug /n -eat bugs").toLowerCase();
+            var prisonLook = prompt("The prison is small, dirty and damp. A rat scurries into a hole in the corner. Bugs squirm and scamper underfoot. There is a barred window to the back. An occupied bed has a stranger asleep snoring to the right. The cell bars and locked door to the front. An empty bed which you had sleeped in the night before to the right. In the center of the dirt floor is a lumpy moldy rug. /n -wake stranger /n - move rug /n - eat bugs").toLowerCase();
             
             if(prisonLook === "eat bugs" || prisonLook === "eat"){
                 
-                var prisonsick = prompt("You find a black millipede runing along the floor, you eat it and instantly regreat it. You have been poisoned and know are puking blook all over the floor. -/look for antidote -/accept fate").toLowerCase(); 
+                var prisonsick = prompt("You find a black millipede runing along the floor, you eat it and instantly regreat it. You have been poisoned and know are puking blook all over the floor. /n - look for antidote /n - accept fate").toLowerCase(); 
                 
                 if(prisonsick == "antidote" || prisonsick === "look for antidote" || prisonsick === "look"){
                     
                     var prisonantidote = prompt("You find a green vile on the floor next to you. /n -use it /n - don't use it").toLowerCase();
                     
                     if(prisonantidote === "use it" || prisonantidote === "use"){
-                        var resume1 = confrim("You drink your roomates sweat it burn in your mouth as you final breath escapes, do you wish to continue?");
+                        var resume = confirm("You drink your roomates sweat it burn in your mouth as you final breath escapes, do you wish to continue?");
                         
-                        if(resume1){
+                        if(resume){
                             Prison();
                         }
                         else{
@@ -85,7 +85,7 @@ function Game(){
             }
             else if(prisonLook === "wake stranger" || prisonLook === "stranger" || prisonLook === "wake"){
                 alert("You wake up a green beast with yellow eyes and sharp teeth he jumps you and stabs you");
-                var resume = confrim("Do you wish to continue?");
+                var resume = confirm("Do you wish to continue?");
                 
                 if(resume){
                         Prison();
@@ -95,7 +95,7 @@ function Game(){
                 }
             }   
             else if(prisonLook === "move rug" || prisonLook === "rug" || prisonLook === "move"){
-                var prisonSword = prompt("You find a sword, what will you use it on? /n -roomate /n -guard").toLowerCase();
+                var prisonSword = prompt("You find a sword, what will you use it on? /n - roomate /n -guard").toLowerCase();
                 inventory.sword++
                     if(prisonSword = "roomate"){
                         var prisonRoomDead = alert("You stabed your rommate to death it looks like a green human seized lizard");
@@ -114,12 +114,12 @@ function Game(){
             }   
          
         }
-        else if(prison === "taunt the guard" || prison === "taunt" || "guard"){
+        else if(prison === "taunt the guard" || prison === "taunt" || prison === "guard"){
             var gaurdTaunt = prompt("How? /n -wistle /n -yell /n -talk.").toLowerCase();
             
-            if(gaurdTanut === "wistle"){
+            if(gaurdTaunt === "wistle"){
                 alert("The guard takes that as an insult, swears at you, and kills you");
-                var resume = comfirm("Do you wish to continue?");
+                var resume = confirm("Do you wish to continue?");
                 
                 if(resume){
                         Prison();
@@ -127,6 +127,19 @@ function Game(){
                 else{
                         alert("WASTED POTENIAL!");
                 }
+            }
+            
+            else if(guardTaunt === "talk"){
+                var guardTalk = prompt("The guard falls to his knees in tears, and begins recitting prayer /n - amen /n -laugh /n - say false doctrine").toLowerCase();
+                
+                if(guardTalk === "amen" || guardTalk === "close"){
+                    var amen = prompt("The guard opens his eyes and stares at you, he gets up, opens the door and leads you out of the prison");
+                }
+            }
+            
+            else if(guardTaunt === "yell"){
+                var guardYell = prompt("The guard screams and runs, he is probaly going to get reinforcements, what should you do /n -hide /n -wait /n - prepare") 
+            
             }
         
         }
